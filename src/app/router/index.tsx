0600914793e9b630c5d936b30353870d9250b8bd
Montaño Flores Diana Erika <dianaerika.montano16@gmail.com>
2026-06-04 14:18:08 -0300

    chore: setup global router and base styles

---
+++ src/app/router/index.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from 'react-router-dom'
+import { RootLayout } from '@/shared/components/RootLayout'
+import { Home } from '@/pages/Home'
+import { NotFound } from '@/pages/NotFound'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Home /> },
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+])
