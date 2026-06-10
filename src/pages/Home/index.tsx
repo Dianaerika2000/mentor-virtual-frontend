84d202949abe7ea17bc474f645ffb48e31354e7b
vivas <vivasiris9@gmail.com>
2026-06-10 12:58:58 +0800

    revision Avatar

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,28 @@
-export function Home() {
+import { Avatar } from "../../features/mentor/components/MentorAvatar/Avatar";
+
+
+// export function Home() {
+//   return (
+//     <div className="flex flex-col items-center justify-center py-20 gap-4">
+//       <h1 className="text-4xl font-bold text-gray-900">
+//         Bienvenido 👋
+//       </h1>
+//       <Avatar />
+//       <p className="text-gray-500 text-lg">
+//         El proyecto está funcionando correctamente.
+//       </p>
+//     </div>
+//   )
+// }
+
+export const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center py-20 gap-4">
-      <h1 className="text-4xl font-bold text-gray-900">
-        Bienvenido 👋
-      </h1>
-      <p className="text-gray-500 text-lg">
-        El proyecto está funcionando correctamente.
-      </p>
-    </div>
-  )
-}
+    <main>
+      <h1>Bienvenidos al Mentor Virtual</h1>
+      
+      <div className="avatar-floating-container">
+        <Avatar />
+      </div>
+    </main>
+  );
+};
